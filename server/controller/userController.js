9051dba0a4d0bd869a8a3e73af7a0a43eefd698d
lawand barbarosa <lawandata14@gmail.com>
2023-08-02 22:10:48 +0300

    userControll done

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,16 +1,44 @@
 const User = require("../model/userModel")
+const jwt = require("jsonwebtoken")
+
+const createToken = async(_id)=>{
+  jwt.sign({_id:_id},process.env.SECRET,{expiresIn:"3d"})
+}
 
 
-const loginUser = (req,res)=>{
+const loginUser = async(req,res)=>{
     const {email,password,username}= req.body;
+
+    try{
+        const user = await User.login(email,password,username)
+
+        const token = await createToken(user._id)
+
+        res.status(200).json({email,token})
+
+    }catch(error){
+     res.status(200).json({error:error.message})
+    }
+
+
 
 
 }
 
 
-const signUpUser = (req,res)=>{
+const signUpUser = async(req,res)=>{
     const {email,password,username}= req.body;
 
+    try{
+        const user = await User.signup(email,password,username)
+
+        const token = await createToken(user._id)
+
+        res.status(200).json({email,token})
+
+    }catch(error){
+    res.status(400).json({error:error.message})
+    }
     
 }
 
